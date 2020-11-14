8d4233829f03481246d574086c3e0089f3b8bbd7
el_le <el_leo_m@hotmail.com>
2020-11-14 23:05:30 +0100

    added currentUser user object

--- src/main/java/com/dbproject/controllers/IndexController.java
+++ src/main/java/com/dbproject/controllers/IndexController.java
@@ -1,27 +1,40 @@
 package com.dbproject.controllers;
 
+import com.dbproject.entities.Users;
 import com.dbproject.repositories.UsersRepository;
 import com.dbproject.repositories.VehiclesRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
 
 
 @Controller
 public class IndexController {
 
-
+    private String currentUserName;
+    private Users currentUser;
     private final VehiclesRepository vehiclesRepository;
+    private final UsersRepository usersRepository;
 
-    public IndexController(UsersRepository usersRepository, VehiclesRepository vehiclesRepository) {
+    public IndexController(VehiclesRepository vehiclesRepository, UsersRepository usersRepository) {
+
         this.vehiclesRepository = vehiclesRepository;
-
+        this.usersRepository = usersRepository;
     }
 
     @RequestMapping({"", "/", "index"})
-    public String getIndex(Model model){
-        model.addAttribute("vehicles" , vehiclesRepository.findAll());
+    public String getIndex(Model model, HttpServletRequest request){
+        Principal principal = request.getUserPrincipal();
+        currentUserName = principal.getName();
+        currentUser = usersRepository.findByUserName(currentUserName);
+        model.addAttribute("currentUser", currentUser);
 
+        model.addAttribute("vehicles" , vehiclesRepository.findAll());
         return "carDealerView/carDealerView";
     }
 
